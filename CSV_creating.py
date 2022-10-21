cba2348bccd5dd81c76ba654e188ae184a6671a0
firuz <gaberud@yandex.ru>
2022-10-21 21:26:42 +0300

    add loggert 2

--- CSV_creating.py
+++ CSV_creating.py
@@ -1,4 +1,4 @@
-def creating ():
+def creating():
     file = 'personal.csv'
-    with open (file, 'w', encoding = 'utf-8') as data:
+    with open(file, 'w', encoding='utf-8') as data:
         data.write(f'Фамилия, Имя, Должность, Отделение, Личный номер;\n')
